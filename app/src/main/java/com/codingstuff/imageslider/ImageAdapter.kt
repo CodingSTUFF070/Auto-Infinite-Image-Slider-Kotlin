1abaffcaea618b6d5d0092ab748abe2cbc22675a
codingstuff070 <codingstuff070@gmail.com>
2022-03-30 10:36:10 +0530

    kotlin

---
+++ app/src/main/java/com/codingstuff/imageslider/ImageAdapter.kt
@@ -0,0 +1,38 @@
+package com.codingstuff.imageslider
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+
+class ImageAdapter(private val imageList: ArrayList<Int>, private val viewPager2: ViewPager2) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView);
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.image_container, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+        if (position == imageList.size-1){
+            viewPager2.post(runnable)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    private val runnable = Runnable {
+        imageList.addAll(imageList)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/codingstuff/imageslider/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.codingstuff.imageslider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  viewPager2: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var imageList:ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable , 2000)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable , 2000)
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer(){
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init(){
+        viewPager2 = findViewById(R.id.viewPager2)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.one)
+        imageList.add(R.drawable.two)
+        imageList.add(R.drawable.three)
+        imageList.add(R.drawable.four)
+        imageList.add(R.drawable.five)
+        imageList.add(R.drawable.six)
+        imageList.add(R.drawable.seven)
+        imageList.add(R.drawable.eight)
+
+
+        adapter = ImageAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+    }
+}
